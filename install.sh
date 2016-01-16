93cace4ee7bf1bfc9eb6587039099af1554b8a34
89berner <89berner@gmail.com>
2016-01-16 18:41:59 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#Getting rootshell:
+
+sudo apt-get install build-essential
+mkdir /opt/rootshell; cd /opt/rootshell;
+wget http://tcpdiag.dl.sourceforge.net/project/rootsh/rootsh/1.5.3/rootsh-1.5.3.tar.gz
+tar -xvzf rootsh-1.5.3.tar.gz
