770453b50727a96cdc8b4462ca8b8a1f2eb2bf07
89berner <89berner@gmail.com>
2016-01-16 19:08:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #Getting rootshell:
-
-sudo apt-get install build-essential
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential;
 mkdir /opt/rootshell; cd /opt/rootshell;
 wget http://tcpdiag.dl.sourceforge.net/project/rootsh/rootsh/1.5.3/rootsh-1.5.3.tar.gz
 tar -xvzf rootsh-1.5.3.tar.gz
+
+Add permissions in line 682 as the third argument
+
+./configure --disable-logfile 
+make
+make install
