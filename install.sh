56dd3bc14c66fcb1c8fe2981c591170bac27b93c
89berner <89berner@gmail.com>
2016-01-17 19:06:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,3 +13,14 @@ make
 make install
 
 mv dockershell /bin/dockershell
+
+#INSTALL https://docs.docker.com/engine/installation/ubuntulinux/
+
+Install sudoers
+
+Cmnd_Alias DOCKERSHELL = /usr/local/bin/rootsh -u nobody /bin/runas
+ALL     ALL=(ALL) NOPASSWD: DOCKERSHELL
+
+#Place dockerrun
+mv dockerrun /bin/dockerrun
+
