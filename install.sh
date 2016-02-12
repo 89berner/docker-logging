47d7c00af0cbfd4ff6be7d12840563f13de17ec2
89berner <89berner@gmail.com>
2016-02-13 00:19:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,27 @@
 #Getting rootshell:
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install build-essential;
-mkdir /opt/rootshell; cd /opt/rootshell;
+sudo apt-get install -y build-essential;
+mkdir /tmp/rootshell; cd /tmp/rootshell;
 wget http://tcpdiag.dl.sourceforge.net/project/rootsh/rootsh/1.5.3/rootsh-1.5.3.tar.gz
 tar -xvzf rootsh-1.5.3.tar.gz
+cd rootsh-1.5.3;
+./configure --disable-logfile && make && sudo make install
 
-Add permissions in line 682 as the third argument
+#INSTALL DOCKER
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list'
+sudo apt-get update
+sudo apt-get purge lxc-docker
+sudo apt-cache policy docker-engine
+sudo apt-get install -y docker-engine
 
-./configure --disable-logfile 
-make
-make install
+#CONFIGURE CONTAINER
+cd /tmp;
+git clone https://github.com/89berner/docker-logging.git
+sudo docker build -t docker_shell docker-logging/
+sudo mv dockershell /bin/dockershell && sudo chmod 755 /bin/dockershell
+sudo mv rundocker /bin/rundocker && sudo chmod 755 /bin/rundocker
+sudo mv sudoers /etc/sudoers
+/usr/sbin/useradd limited -s /bin/bash
 
-mv dockershell /bin/dockershell
-
-#INSTALL https://docs.docker.com/engine/installation/ubuntulinux/
-
-Install sudoers
-
-Cmnd_Alias DOCKERSHELL = /usr/local/bin/rootsh -u nobody /bin/runas
-ALL     ALL=(ALL) NOPASSWD: DOCKERSHELL
-
-#Place dockerrun
-mv dockerrun /bin/dockerrun
-
-docker build .
-
-docker build -t docker_shell .
-
-limited:x:1002:1002::/home/limited:/bin/bash
